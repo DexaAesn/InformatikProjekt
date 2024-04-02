b0671fd48c4be9f285f5a01aff5f4c139307e4f0
Dexa Aesn <kirchner.marlene@dsgenf.ch>
2024-04-02 19:48:19 +0200

    added highscore and timer

--- Assets/Scripts/Player_Score.cs
+++ Assets/Scripts/Player_Score.cs
@@ -1,20 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Player_Score : MonoBehaviour
 {
     public float timeLeft = 120;
-    public int playerScore;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    public int playerScore = 0;
+    public GameObject timeLeftUI;
+    public GameObject playerScoreUI;
     void Update()
     {
         timeLeft -= Time.deltaTime;
+        timeLeftUI.gameObject.GetComponent<TextMeshProUGUI>().text = ("Time Left: " + (int)timeLeft);
+        playerScoreUI.gameObject.GetComponent<TextMeshProUGUI>().text = ("Score: " + playerScore);
+        if (timeLeft < 0.1f)
+        {
+            SceneManager.LoadScene ("Menu");
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D trig) 
+    {
+        CountScore();    
+    }
+
+    void CountScore()
+    {
+        playerScore = playerScore + (int)(timeLeft * 10);
+        Debug.Log(playerScore);
     }
 }
