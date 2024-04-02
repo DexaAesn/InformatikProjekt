f8351b6fc592a0b9f0c78cdbcdc255479ce8c0d5
Dexa Aesn <kirchner.marlene@dsgenf.ch>
2024-04-02 15:35:39 +0200

    Player moves

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,18 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Vector2 movement;
+    private Rigidbody2D rb;
+
+    private void Awake()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+    }
+    private void OnMovement (InputValue value)
+    {
+        movement = value.Get<Vector2>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate() 
     {
-        
+        rb.MovePosition(rb.position + movement * Time.fixedDeltaTime);
     }
 }
